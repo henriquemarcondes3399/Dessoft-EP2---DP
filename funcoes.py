7f48b831f77cb5c885ad939e661a5f6e7e8e1f5f
Henrique Marcondes <hmarcondes1979@gmail.com>
2026-07-28 12:12:47 -0400

    segundo codigo

--- funcoes.py
+++ funcoes.py
@@ -6,4 +6,45 @@ def transforma_base(questoes):
             base[nivel] = []
         base[nivel].append(questao)
     return base
+def valida_questao(questao):
+    erros = {}
+
+    for chave in ['titulo', 'nivel', 'opcoes', 'correta']:
+        if chave not in questao:
+            erros[chave] = 'nao_encontrado'
+
+    if len(questao) != 4:
+        erros['outro'] = 'numero_chaves_invalido'
+
+    if 'titulo' in questao:
+        if questao['titulo'].strip() == '':
+            erros['titulo'] = 'vazio'
+
+    if 'nivel' in questao:
+        if questao['nivel'] not in ['facil', 'medio', 'dificil']:
+            erros['nivel'] = 'valor_errado'
+
+    if 'opcoes' in questao:
+        opcoes = questao['opcoes']
+        if len(opcoes) != 4:
+            erros['opcoes'] = 'tamanho_invalido'
+        else:
+            todas_existem = True
+            for letra in ['A', 'B', 'C', 'D']:
+                if letra not in opcoes:
+                    todas_existem = False
+
+            if not todas_existem:
+                erros['opcoes'] = 'chave_invalida_ou_nao_encontrada'
+            else:
+                vazias = {}
+                for letra in ['A', 'B', 'C', 'D']:
+                    if opcoes[letra].strip() == '':
+                        vazias[letra] = 'vazia'
+                if vazias != {}:
+                    erros['opcoes'] = vazias
+    if 'correta' in questao:
+        if questao['correta'] not in ['A', 'B', 'C', 'D']:
+            erros['correta'] = 'valor_errado'
+    return erros
 
